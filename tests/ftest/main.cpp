775dcd52acaa804cb6a78cc7489f85e765aedb94
chope <100432481@alumnos.uc3m.es>
2025-03-26 19:16:30 +0100

    Fix minor issues in funcional tests

--- tests/ftest/main.cpp
+++ tests/ftest/main.cpp
@@ -32,23 +32,27 @@ inline std::filesystem::path const current_path =
 
 namespace {
 
-sim::error_code run_sim(std::span<char const*> const args) {
+auto run_sim(std::span<char const*> const args) {
   auto const init = std::chrono::high_resolution_clock::now();
 
-  sim::parse_arguments(args)
-      .and_then(sim::run_simulation)
-      .and_then(sim::write_output)
-      .or_else([](std::runtime_error const& error) -> sim::err::expected<sim::Simulation> {
-        std::println("Error: {}", error.what());
-        return sim::err::unexpected(std::format("Simulation Failed", error.what()));
-      });
+  auto result = //
+      sim::parse_arguments(args)
+          .and_then(sim::run_simulation)
+          .and_then(sim::write_output)
+          .transform_error([](std::runtime_error const& error) -> std::string {
+            return std::format("Simulation Failed!\n Error: {}", error.what());
+          });
 
-  auto const end                                       = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<sim::math::scalar> const total = end - init;
+  if (not result) {
+    MESSAGE("{}", result.error());
+    return -1;
+  }
+
+  std::chrono::duration<sim::math::scalar> const total = std::chrono::high_resolution_clock::now() - init;
   std::cout << "Execution time: " << total << "\n";
   std::cout << "---------------------------------------\n";
 
-  return sim::success;
+  return 0;
 }
 
 bool compare_files(std::string const& file1, std::string const& file2) {
@@ -56,11 +60,11 @@ bool compare_files(std::string const& file1, std::string const& file2) {
   std::ifstream f2(file2, std::ios::binary);
 
   if (!f1 || !f2) {
-    DOCTEST_MESSAGE("Couldn't open test files for comparing them...");
+    MESSAGE("Couldn't open test files for comparing them...");
     return false;
   }
 
-  DOCTEST_INFO(std::format("Comparing files: {} - {}", file1, file2));
+  INFO(std::format("Comparing files: {} - {}", file1, file2));
   return std::equal(std::istreambuf_iterator(f1), std::istreambuf_iterator<char>(), std::istreambuf_iterator(f2));
 }
 
@@ -73,12 +77,17 @@ void test_simulation(std::string_view file_name, sim::i8 iterations) {
   std::string const it            = std::to_string(iterations);
 
   std::array args {"binary_name", it.c_str(), input_file.c_str(), out_file.c_str()};
-  DOCTEST_CHECK(run_sim(args) == sim::error_code::success);
-  DOCTEST_CHECK(compare_files(out_file, expected_file));
+
+  REQUIRE(run_sim(args) == sim::error_code::success);
+
+  CHECK(compare_files(out_file, expected_file));
   REQUIRE(std::filesystem::remove_all(tmp_dir));
 }
 
 } // namespace
+
+
+DOCTEST_TEST_SUITE_BEGIN("Integration tests");
 
 TEST_CASE("Large file") {
   for (sim::i8 i = 1; i <= 5; ++i)
@@ -89,3 +98,5 @@ TEST_CASE("Small file") {
   for (sim::i8 i = 1; i <= 5; ++i)
     test_simulation("small", i);
 }
+
+DOCTEST_TEST_SUITE_END();

8ebca8982bc7a1f4c06eabf7b78b5d905e9e0f04
alvcabre <100432481@alumnos.uc3m.es>
2025-04-07 23:20:41 +0200

    Refactor soa to zip moved to its own file

--- src/include/rflect/containers/proxy.hpp
+++ src/include/rflect/containers/proxy.hpp
@@ -13,7 +13,7 @@
 
 #pragma once
 
-#include <rflect/utility/static_array.hpp>
+#include <rflect/converters/soa_to_zip.hpp>
 
 #define PARENS ()
 
@@ -49,51 +49,6 @@
   }
 
 namespace rflect {
-
-namespace detail {
-template<typename T>
-consteval auto type_soa_zipview() {
-
-  constexpr auto raw_members = nonstatic_data_members_of(^^T) | std::views::transform(std::meta::type_of) |
-                               std::views::transform(std::meta::remove_cvref) | to_static_array();
-
-
-  std::vector<std::meta::info> value_members;
-
-  template for (constexpr auto member: raw_members) {
-    auto new_member = add_lvalue_reference(^^typename[:member:] ::value_type const);
-    // auto const_member = add_const(new_member);
-    value_members.push_back(new_member);
-  }
-
-  return substitute(^^std::tuple, value_members);
-}
-
-template<typename To, typename From, std::meta::info... members>
-constexpr auto soa_to_zip_helper(From const& from, std::size_t const index) -> To {
-
-  auto value = std::views::zip(from.[:members:]...);
-  return value[index];
-}
-
-template<typename From>
-consteval auto get_soa_to_zip_helper() {
-  using To = [:type_soa_zipview<From>():];
-
-  std::vector args = {^^To, ^^From};
-  for (auto mem: nonstatic_data_members_of(^^From)) {
-    args.push_back(reflect_value(mem));
-  }
-
-  return extract<To (*)(From const&, std::size_t const index)>(substitute(^^soa_to_zip_helper, args));
-}
-
-template<typename From>
-constexpr auto soa_to_zip(From const& from, std::size_t const index) {
-  return get_soa_to_zip_helper<From>()(from, index);
-}
-
-} // namespace detail
 
 template<template<typename> class Proxy, class Container>
 class proxy_base {
